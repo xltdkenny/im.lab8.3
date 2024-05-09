af1fc3e2a72581538126e6fbbf55483cd007740d
kotovskij531@gmail.com <kotovskij531@gmail.com>
2024-05-09 15:05:20 +0700

    Добавьте файлы проекта.

---
+++ GenerationEvent/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace GenerationEvent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.probabilitiesDataGridView = new System.Windows.Forms.DataGridView();
+            this.runExperimentButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.Event = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Probability = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.probabilitiesDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // probabilitiesDataGridView
+            // 
+            this.probabilitiesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.probabilitiesDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Event,
+            this.Probability});
+            this.probabilitiesDataGridView.Location = new System.Drawing.Point(429, 32);
+            this.probabilitiesDataGridView.Name = "probabilitiesDataGridView";
+            this.probabilitiesDataGridView.Size = new System.Drawing.Size(344, 343);
+            this.probabilitiesDataGridView.TabIndex = 0;
+            // 
+            // runExperimentButton
+            // 
+            this.runExperimentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.runExperimentButton.Location = new System.Drawing.Point(71, 304);
+            this.runExperimentButton.Name = "runExperimentButton";
+            this.runExperimentButton.Size = new System.Drawing.Size(180, 71);
+            this.runExperimentButton.TabIndex = 1;
+            this.runExperimentButton.Text = "Сделать эксперимент";
+            this.runExperimentButton.UseVisualStyleBackColor = true;
+            this.runExperimentButton.Click += new System.EventHandler(this.runExperimentButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(71, 395);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(702, 256);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(71, 32);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(303, 257);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // Event
+            // 
+            this.Event.DataPropertyName = "Event";
+            this.Event.HeaderText = "Event";
+            this.Event.Name = "Event";
+            this.Event.Width = 200;
+            // 
+            // Probability
+            // 
+            this.Probability.DataPropertyName = "Probability";
+            this.Probability.HeaderText = "Probability";
+            this.Probability.Name = "Probability";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 663);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.runExperimentButton);
+            this.Controls.Add(this.probabilitiesDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.probabilitiesDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView probabilitiesDataGridView;
+        private System.Windows.Forms.Button runExperimentButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Event;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Probability;
+    }
+}
+
---
+++ GenerationEvent/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerationEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RunExperiments();
+            AddDataToDataGridView();
+        }
+
+        private void AddDataToDataGridView()
+        {
+            // Добавление строк с данными
+            string[] row0 = { "Все будет отлично", "0,20" };
+            string[] row1 = { "Все будет хорошо", "0,20" };
+            string[] row2 = { "Все будет неплохо", "0,10" };
+            string[] row3 = { "Все будет плохо", "0,10" };
+            string[] row4 = { "Все будет отвратительно", "0,25" };
+            string[] row5 = { "Все будет ужасно отвратительно", "0,15" };
+
+            probabilitiesDataGridView.Rows.Add(row0);
+            probabilitiesDataGridView.Rows.Add(row1);
+            probabilitiesDataGridView.Rows.Add(row2);
+            probabilitiesDataGridView.Rows.Add(row3);
+            probabilitiesDataGridView.Rows.Add(row4);
+            probabilitiesDataGridView.Rows.Add(row5);
+        }
+
+        private void RunExperiments()
+        {
+            int[] N = new int[] { 10, 100, 1000, 10000 };  // Разные значения N
+            List<Tuple<string, double>> events = new List<Tuple<string, double>>();
+            foreach (DataGridViewRow row in probabilitiesDataGridView.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null)
+                {
+                    events.Add(Tuple.Create(row.Cells[0].Value.ToString(), Convert.ToDouble(row.Cells[1].Value)));
+                }
+            }
+
+            StringBuilder results = new StringBuilder();
+            foreach (var n in N)
+            {
+                results.AppendLine($"Эксперимент с N = {n}:");
+                var counts = new Dictionary<string, int>();
+                foreach (var evt in events)
+                {
+                    counts[evt.Item1] = 0;
+                }
+
+                Random rand = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    double r = rand.NextDouble();
+                    double cumulative = 0;
+                    foreach (var evt in events)
+                    {
+                        cumulative += evt.Item2;
+                        if (r < cumulative)
+                        {
+                            counts[evt.Item1]++;
+                            break;
+                        }
+                    }
+                }
+
+                foreach (var evt in events)
+                {
+                    double empiricalProbability = (double)counts[evt.Item1] / n;
+                    results.AppendLine($"Событие {evt.Item1}: Теоретическая = {evt.Item2}, Эмпирическая = {empiricalProbability}");
+                }
+                results.AppendLine();
+                chart1.Series.Clear();
+                foreach (var evt in events)
+                {
+                    var series = chart1.Series.Add(evt.Item1);
+                    series.Points.AddXY("Теоретическая", evt.Item2);
+                    double empiricalProbability = (double)counts[evt.Item1] / N.Last();
+                    series.Points.AddXY("Эмпирическая", empiricalProbability);
+                }
+
+            }
+            resultTextBox.Text = (results.ToString());
+
+        }
+
+        private void runExperimentButton_Click(object sender, EventArgs e)
+        {
+            RunExperiments();
+        }
+    }
+}
